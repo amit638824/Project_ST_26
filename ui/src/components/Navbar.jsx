fe95e826d82e456e81c0b6bd7508c5a648e20737
ad01m <ad74mi@gmail.com>
2026-07-22 09:03:32 +0530

    img

--- ui/src/components/Navbar.jsx
+++ ui/src/components/Navbar.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
     return (<>
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+<div className='row'>
+  <div className='col-sm-2'></div>
+  <div className='col-sm-8 menu'>
+    <nav className="navbar navbar-expand-lg navbar-light">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">
-      Navbar
+      Freelancing <b className='text-color1'>24X7</b>
     </a>
     <button
       className="navbar-toggler"
@@ -27,67 +31,42 @@ const Navbar = () => {
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">
-            Link
+            About Us
           </a>
-        </li>
-        <li className="nav-item dropdown">
-          <a
-            className="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li>
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </li>
-          </ul>
         </li>
         <li className="nav-item">
-          <a
-            className="nav-link disabled"
-            href="#"
-            tabIndex={-1}
-            aria-disabled="true"
-          >
-            Disabled
+          <a className="nav-link" href="#">
+            Services
           </a>
         </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Pricing
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Login
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Register
+          </a>
+        </li>
+        
       </ul>
-      <form className="d-flex">
-        <input
-          className="form-control me-2"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button className="btn btn-outline-success" type="submit">
-          Search
-        </button>
-      </form>
     </div>
   </div>
 </nav>
-
+  </div>
+  <div className='col-sm-2'></div>
+</div>
     </>)
 }
 
