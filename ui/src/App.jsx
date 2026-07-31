8ec35fe1c624a7bb415efabea84cca0232320488
ad <ad74mi@gmail.com>
2026-07-31 09:31:09 +0530

    new

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { use, useEffect, useState } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './App.css';
@@ -11,7 +11,7 @@ import Pricing from './components/Pricing';
 import Contactus from './components/Contactus';
 import Aboutus from './components/Aboutus';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';  
+import Footer from './components/Footer';
 import AdminDashboard from './components/admin/AdminDashboard';
 import AdminProjects from './components/admin/AdminProjects';
 import AdminUsers from './components/admin/AdminUsers';
@@ -31,42 +31,57 @@ import UserProfile from './components/user/UserProfile';
 import UserPlans from './components/user/UserPlans';
 
 const App = () => {
+  const location = useLocation();
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const info = JSON.parse(localStorage.getItem('info'))
+    setUser(info)
+  }, [location])
+
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          {/* common url */}
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/services' element={<Services />} />
-          <Route path='/pricing' element={<Pricing />} />
-          <Route path='/contact-us' element={<Contactus />} />
-          <Route path='/about-us' element={<Aboutus />} />
-          {/* admin url */}
-          <Route path='/admin-dashboard' element={<AdminDashboard/>} /> 
-          <Route path='/admin-project' element={<AdminProjects/>} />
-          <Route path='/admin-users' element={<AdminUsers/>} />
-          <Route path='/admin-clients' element={<AdminClients/>} />
-          <Route path='/admin-bids' element={<AdminBids/>} />
-          <Route path='/admin-profile' element={<AdminProfile/>} />
-          <Route path='/admin-plans' element={<AdminPlans/>} />
-          {/* client url */}
-          <Route path='/client-dashboard' element={<ClientDashboard/>} />
-          <Route path='/client-post-projects' element={<ClientPostProject/>} />
-          <Route path='/client-manage-projects' element={<ClientManageProjects/>} />
-          <Route path='/client-review-bids' element={<ClientReviewBids/>} />
-          <Route path='/client-profile' element={<ClientProfile/>} />
-          {/* users url */}
-          <Route path='/user-dashboard' element={<UserDashboard/>} />
-          <Route path='/user-project' element={<UserProjects/>} />
-          <Route path='/user-bids' element={<UserBids/>} />
-          <Route path='/user-profile' element={<UserProfile/>} />
-          <Route path='/user-plans' element={<UserPlans/>} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <Navbar />
+      <Routes>
+        {/* common url */}
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/contact-us' element={<Contactus />} />
+        <Route path='/about-us' element={<Aboutus />} />
+        {/* admin url */}
+        {user?.type == 'admin' && <>
+          <Route path='/admin-dashboard' element={<AdminDashboard />} />
+          <Route path='/admin-project' element={<AdminProjects />} />
+          <Route path='/admin-users' element={<AdminUsers />} />
+          <Route path='/admin-clients' element={<AdminClients />} />
+          <Route path='/admin-bids' element={<AdminBids />} />
+          <Route path='/admin-profile' element={<AdminProfile />} />
+          <Route path='/admin-plans' element={<AdminPlans />} />
+        </>}
+
+        {/* client url */}
+        {user?.type == 'client' && <>
+          <Route path='/client-dashboard' element={<ClientDashboard />} />
+          <Route path='/client-post-projects' element={<ClientPostProject />} />
+          <Route path='/client-manage-projects' element={<ClientManageProjects />} />
+          <Route path='/client-review-bids' element={<ClientReviewBids />} />
+          <Route path='/client-profile' element={<ClientProfile />} />
+        </>}
+
+        {/* users url */}
+        {user?.type == 'user' && <>
+          <Route path='/user-dashboard' element={<UserDashboard />} />
+          <Route path='/user-project' element={<UserProjects />} />
+          <Route path='/user-bids' element={<UserBids />} />
+          <Route path='/user-profile' element={<UserProfile />} />
+          <Route path='/user-plans' element={<UserPlans />} />
+        </>}
+        {/* Not found page */}
+        <Route path='*' element={<h1 className='text-center text-danger p-5 m-5'>404 Page Not Found</h1>} />
+      </Routes>
+      <Footer />
     </>
   );
 };
